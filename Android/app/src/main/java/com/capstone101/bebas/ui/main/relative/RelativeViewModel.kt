e9a6e7afbf344f39e42abafa5cd48ac7ce0f4138
ruman1609 <rudyrachman16@gmail.com>
2021-06-01 18:35:05 +0800

    Parcelized User

--- Android/app/src/main/java/com/capstone101/bebas/ui/main/relative/RelativeViewModel.kt
+++ Android/app/src/main/java/com/capstone101/bebas/ui/main/relative/RelativeViewModel.kt
@@ -1,12 +1,15 @@
 package com.capstone101.bebas.ui.main.relative
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import com.capstone101.core.domain.model.Relatives
 import com.capstone101.core.domain.model.User
 import com.capstone101.core.domain.usecase.IUseCase
 
 class RelativeViewModel(private val useCase: IUseCase) : ViewModel() {
     fun getRelative(callback: (Relatives) -> Unit) = useCase.getRelative { callback(it) }
+
+    fun searchUser(username: String) = useCase.getUserSearch(username).asLiveData()
 
     fun invite(relatives: Relatives, target: User, condition: Boolean) =
         useCase.invitingRelative(relatives, target, condition)
