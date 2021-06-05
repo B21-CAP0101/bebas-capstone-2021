6260815c95df147117ecd0063a5c6ebaefbf2107
isekaiweb <cobasaja271@gmail.com>
2021-06-05 22:04:38 +0700

    removed unecessary thing

--- Android/app/src/main/java/com/capstone101/bebas/ui/main/home/HomeFragment.kt
+++ Android/app/src/main/java/com/capstone101/bebas/ui/main/home/HomeFragment.kt
@@ -11,7 +11,6 @@ import android.media.MediaRecorder
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
